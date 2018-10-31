a479d6811d9ef345f29f7122e595a8a4fc139cd4
ash <ash@ash.miracloud.ru>
2018-10-31 14:20:30 +0300

    update config

--- go-smokeping.go
+++ go-smokeping.go
@@ -1,6 +1,6 @@
 // 
 // Written by Romanenko Denys <romanenkodenys@gmail.com>
-
+//  
 package main
 
 // Import section ----------------------------------------------------------------------------------------
@@ -8,9 +8,11 @@ package main
 import (  
 //    "github.com/influxdata/influxdb/client/v2"
     "github.com/BurntSushi/toml"
+    "github.com/sparrc/go-ping"
     "fmt"
     "flag"
-//    "log"
+    "errors"
+    "log"
 //    "os"
 //    "bufio"
 //    "os/exec"
@@ -18,18 +20,25 @@ import (
 //    "time"
 //    "strconv"
 )
+// Usage ------------------------------------------------------------------------------------------------
+var usage = `
+Usage:
+
+    go-smokeping [-config configfile.toml] [-verbose]
+`
 
 // Config file structure --------------------------------------------------------------------------------
 type Config struct {
     Global_tags globalTags
     Agent agentInfo
-    Database databaseConnector
-    Probes []probe `toml:"probe"`
+    Database map[string] databaseConnector
+    Probes map[string] probe `toml:"probe"`
     Groups []group `toml:"group"`
 }
  
 type globalTags struct {
     Project string
+    Output string
 }
 
 type agentInfo struct {
@@ -48,6 +57,7 @@ type databaseConnector struct {
 type host struct {
     Ip string
     Fqdn string
+    Probe string
 }
 
 type group struct {
@@ -56,26 +66,56 @@ type group struct {
 }
 
 type probe struct {
-    Name string
     Retries int
     Packet int
 }
 
 
-// Get command line parameters
-
+// Get command line parameters --------------------------------------------------------------------------
 func GetCommandLineArgs() (string,bool) {
     var (
 	configfile string
 	verbose bool
     )
 // Get command line args
-
     flag.StringVar(&configfile, "config","go-smokeping.toml","Config file locaion")
     flag.BoolVar(&verbose, "verbose",false,"true/false")
+    flag.Usage = func() {
+	fmt.Printf(usage)
+    }
     flag.Parse()
+
     return configfile, verbose
 }
+
+// Return probe by name ---------------------------------------------------------------
+func GetProbe(config Config, probe_name string) (probe, error) {
+    var(
+	 pr probe
+	 ok bool
+    )
+
+    if pr, ok = config.Probes[probe_name]; ok {
+	return pr, nil
+    } else {
+	return pr,errors.New("Probe "+probe_name+" not found")
+    }
+}
+
+// ping host --------------------------------------------------------------------------
+func PingHost(host string, retries int, packetsize int) {
+    pinger, err := ping.NewPinger(host)
+    if err != nil {
+        log.Print(err)
+	return
+    }
+    pinger.SetPrivileged(true)
+    pinger.Count = retries
+    pinger.Run() // blocks until finished
+    stats := pinger.Statistics()
+    fmt.Printf("%v",stats)
+}
+
 //-------------------------------------------------------------------------------------------------------
 
 func main() { 
@@ -89,11 +129,18 @@ func main() {
 	return
     }
 
-    fmt.Printf("Connector:%s %s\n", config.Database.Type ,config.Database.Host)
     for _,g := range config.Groups {
-        fmt.Printf("Group: %v\n", g.Name)
         for _, h := range g.Hosts {
-	    fmt.Printf("\tHost %s - %s\n", h.Fqdn, h.Ip)
+	    probe , err:= GetProbe(config, h.Probe)
+	    if err != nil {
+		log.Printf("%s for host %s",err.Error(),h.Fqdn)
+		continue
+	    }
+	    log.Printf("Probing host %s(%s) with probe %s\n", h.Fqdn, h.Ip, h.Probe)
+	    
+		if (h.Probe == "icmp") {
+		    PingHost(h.Ip, probe.Retries, probe.Packet)
+		}
 	}
     }
 }
